b65e7931ebd170628a8cade2be9b8f7fb2e6c7b9
M Difa Ananda <difacool40@gmail.com>
2023-02-07 22:53:18 +0800

    add: deploy site to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://difaananda40.github.io',
+  base: '/astro-web-profile',
 });
