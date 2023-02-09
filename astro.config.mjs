a580201c27ade098cacb3659920dbe9928d9b5ee
M Difa Ananda <difacool40@gmail.com>
2023-02-09 10:57:04 +0800

    change: use env config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://difaananda40.github.io',
-  base: '/astro-web-profile',
+  site: import.meta.env.SITE || '',
+  base: import.meta.env.BASE_URL || '',
 });
