171c17365721677344ae5782b7b9a76b621218e8
M Difa Ananda <difacool40@gmail.com>
2023-03-01 03:36:10 +0800

    add: partytown for google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,23 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  ...(import.meta.env.SITE && { site: import.meta.env.SITE }),
-  ...(import.meta.env.BASE_URL && { base: import.meta.env.BASE_URL }),
+  integrations: [
+    tailwind(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
+  ...(import.meta.env.SITE && {
+    site: import.meta.env.SITE
+  }),
+  ...(import.meta.env.BASE_URL && {
+    base: import.meta.env.BASE_URL
+  })
 });
