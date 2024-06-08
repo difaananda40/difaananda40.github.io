f0d95c3aebab69a2580e47975c2ea8dedcaf5ef9
difaananda40 <difacool40@gmail.com>
2024-06-09 03:14:16 +0800

    feat: remove partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,10 @@
-import { defineConfig } from 'astro/config';
-
 // https://astro.build/config
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-import partytown from "@astrojs/partytown";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
-    partytown({
-      // Adds dataLayer.push as a forwarding-event.
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
   ],
   ...(import.meta.env.SITE && {
     site: import.meta.env.SITE
